d9dcacd0abac4e28aa74ef1543fdd1b90da7ac5c
MohsinKhan747 <mohsinkhan.0787@gmail.com>
2025-06-05 23:03:13 +0500

    Initial Commit

---
+++ OyeHoye/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  OyeHoye
+//
+//  Created by Mohsin Khan on 05/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OyeHoye/OyeHoyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OyeHoyeApp.swift
+//  OyeHoye
+//
+//  Created by Mohsin Khan on 05/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct OyeHoyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
