67f0949082096335b60827ae36552421d0843d29
MohsinKhan747 <mohsinkhan.0787@gmail.com>
2025-06-23 17:04:54 +0500

    newCommit

--- OyeHoye/ContentView.swift
+++ OyeHoye/ContentView.swift
@@ -10,11 +10,55 @@ import SwiftUI
 struct ContentView: View {
     @State var tapCount = 0
     var body: some View {
+        //        LinearGradient(stops: [
+        ////            Gradient.Stop(color: .white,location: 0.45),
+        ////            Gradient.Stop(color: .black,location: 0.55)
+        //            .init(color: .white,location: 0.45),
+        //            .init(color: .black,location: 0.55),
+        //        ], startPoint: .top, endPoint: .bottom)
+        //        RadialGradient(colors: [.blue,.black], center: .center, startRadius: 20, endRadius: 200)
         
-        Button("TapCount: \(tapCount)"){
-            tapCount += 1
-        }
+        //    Text("Your Content")
+        //            .frame(maxWidth:.infinity, maxHeight: .infinity)
+        //            .foregroundStyle(.white)
+        //            .background(.red.gradient)
+        //    }
+//        
+//        Button("Delete Selection", role:.destructive ,action: executeDelete)
+        
+//        VStack{
+//            Button("Button 1"){}
+//                .buttonStyle(.bordered)
+//            Button("Button 2",role:.destructive){}
+//                .buttonStyle(.bordered)
+//            Button("Button 3"){}
+//                .buttonStyle(.borderedProminent)
+//                .tint(.indigo)
+//
+//            Button("Button 4",role:.destructive){}
+//                .buttonStyle(.borderedProminent)
+//
+//
+//        }
+        
+//        Button{
+//            print("Button was tapped")
+//        } label: {
+//            Text("Tap me !!")
+//                .padding()
+//                .foregroundStyle(.white)
+//                .background(.red)
+//        }
+        
+        Image(systemName: "pencil.circle")
+            .foregroundStyle(.red)
+            .font(.largeTitle)
+        
     }
+    func executeDelete(){
+        print("Now Deleting...")
+    }
+
 }
 
 #Preview {
