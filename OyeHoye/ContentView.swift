142f274c64fe2bd4a4bd08d3b57566ad5a6a128d
MohsinKhan747 <mohsinkhan.0787@gmail.com>
2025-06-23 18:13:37 +0500

    Alert message

--- OyeHoye/ContentView.swift
+++ OyeHoye/ContentView.swift
@@ -8,57 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tapCount = 0
+    @State private var showingAlert  = false
+    
     var body: some View {
-        //        LinearGradient(stops: [
-        ////            Gradient.Stop(color: .white,location: 0.45),
-        ////            Gradient.Stop(color: .black,location: 0.55)
-        //            .init(color: .white,location: 0.45),
-        //            .init(color: .black,location: 0.55),
-        //        ], startPoint: .top, endPoint: .bottom)
-        //        RadialGradient(colors: [.blue,.black], center: .center, startRadius: 20, endRadius: 200)
+        // *************  Alerts ************************
+        // create an alert and set the condition to be showwn
+        // has a title and a button which is there to dismiss it
+        Button("Show Alert"){
+            showingAlert = true
+        }
+        .alert("Important Message",isPresented: $showingAlert) {
+            Button("OK"){}
+        }
         
-        //    Text("Your Content")
-        //            .frame(maxWidth:.infinity, maxHeight: .infinity)
-        //            .foregroundStyle(.white)
-        //            .background(.red.gradient)
-        //    }
-//        
-//        Button("Delete Selection", role:.destructive ,action: executeDelete)
-        
-//        VStack{
-//            Button("Button 1"){}
-//                .buttonStyle(.bordered)
-//            Button("Button 2",role:.destructive){}
-//                .buttonStyle(.bordered)
-//            Button("Button 3"){}
-//                .buttonStyle(.borderedProminent)
-//                .tint(.indigo)
-//
-//            Button("Button 4",role:.destructive){}
-//                .buttonStyle(.borderedProminent)
-//
-//
-//        }
-        
-//        Button{
-//            print("Button was tapped")
-//        } label: {
-//            Text("Tap me !!")
-//                .padding()
-//                .foregroundStyle(.white)
-//                .background(.red)
-//        }
-        
-        Image(systemName: "pencil.circle")
-            .foregroundStyle(.red)
-            .font(.largeTitle)
         
     }
-    func executeDelete(){
-        print("Now Deleting...")
-    }
-
 }
 
 #Preview {
