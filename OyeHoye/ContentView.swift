b43b013ad1c37cd0cc133586d87aa02aad6d89f0
MohsinKhan747 <mohsinkhan.0787@gmail.com>
2025-06-26 18:23:23 +0500

    Terniary Operator instead of using traditional conditional statement

--- OyeHoye/ContentView.swift
+++ OyeHoye/ContentView.swift
@@ -8,82 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var countries = ["Estonia","France", "Germany", "Ireland","Italy","Nigeria", "Poland","Spain", "UK", "Ukraine", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var useRedText = false
     
-    @State private var showingScore = false
-    @State private var scoreTitle: String = ""
-    
-    @State private var userScore = 0
-    @State private var questionDisplayed = 0
-    @State private var restartTitle = ""
+    // using terniary operator for this instead of making and destroying button using the tradional conditional statement
     
     var body: some View {
-        ZStack{
-            RadialGradient(stops: [
-                .init(color: Color(red:0.1, green:0.2, blue: 0.45), location:0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue:0.26) , location: 0.3)
-            ], center: .top, startRadius: 200, endRadius: 700)
-                .ignoresSafeArea()
-            VStack{
-                Spacer()
-                Text("Guess the Flag")
-                    .font(.largeTitle.bold())
-                    .foregroundStyle(.white)
-                VStack(spacing:30){
-                    VStack{
-                        Text("Tap the flag of")
-                            .foregroundStyle(.secondary)
-                            .font(.subheadline.weight(.heavy))
-                        Text(countries[correctAnswer])
-                            .font(.largeTitle.weight(.semibold))
-                        
-                    }
-                    ForEach(0..<3){ number in
-                        Button{
-                            flagTapped(number)
-                        }label: {
-                            Image(countries[number])
-                                .shadow(radius: 5)
-                            
-                            
-                        }
-                    }
-                    
-                }
-                .frame(maxWidth:.infinity)
-                .padding(.vertical,20)
-                .background(.regularMaterial)
-                .clipShape(.rect(cornerRadius:20))
-                
-                Spacer()
-                Spacer()
-                
-                Text("Score: \(userScore)")
-                    .foregroundStyle(.white)
-                    .font(.title.bold())
-            }
-            .padding()
-            .alert(scoreTitle,isPresented: $showingScore){
-                Button("Continue",action: askQuestion)
-            } message: {
-            }
-            
+        Button("Hello World!"){
+            useRedText.toggle()
         }
-    }
-    func flagTapped(_ number:Int){
-        if number == correctAnswer{
-            //scoreTitle = "Correct"
-            userScore += 1
-        }else{
-            scoreTitle = "Wrong! That's the flag of \(countries[number])"
-            userScore -= 1
-        }
-        showingScore = true
-    }
-    func askQuestion(){
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        .foregroundStyle(useRedText ? .red : .blue)
+        
     }
 }
 
