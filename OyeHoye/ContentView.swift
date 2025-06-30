0bfa6bb709d422b59d097a909175bc8ea4749ed0
MohsinKhan747 <mohsinkhan.0787@gmail.com>
2025-06-30 20:44:26 +0500

    Stepper

--- OyeHoye/ContentView.swift
+++ OyeHoye/ContentView.swift
@@ -8,18 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
-     // Environmental variable
-        // changing
-        
-        VStack{
-            Text("First Text")
-                .font(.largeTitle)
-            Text("Middle Text")
-            Text("Last Text")
-        }
-        .font(.title)
+        Stepper("\(sleepAmount) hourse", value: $sleepAmount)
+    
     }
 }
 
